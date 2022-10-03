cd3f0af417ee239422a4f8216e380f948dd9187c
Starrymag <starrymag101@gmail.com>
2022-10-03 21:00:26 +0300

    init commit

---
+++ tictac.py
@@ -0,0 +1,27 @@
+import tkinter as tk
+
+class Game:
+    def __init__(self):
+        field = [[' '] * 3] * 3
+        window = tk.Tk()
+
+
+    def input_motion(self):
+        pass
+
+
+    def proccesing(self):
+        pass
+
+
+    def draw(self):
+        pass
+
+
+    def start_game(self):
+        pass
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.start_game()
